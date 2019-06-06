b3850d4efe2a3d936b3c734d7939a18ac3a41397
John Liu <johnliutw@hotmail.com>
2019-06-06 14:11:32 +0800

    adjust SBL player shooting & minute data

--- app/models/sbl_player_datum.rb
+++ app/models/sbl_player_datum.rb
@@ -1,4 +1,23 @@
 class SblPlayerDatum < ApplicationRecord
   belongs_to :sbl_player, :foreign_key => "player_id"
   belongs_to :sbl_team, :foreign_key => "team_id"
+  after_initialize :set_fga, :set_3pa, :set_fta, :set_sp
+
+  def set_fga
+    self.fga = self.fga + self.fg
+  end
+
+  def set_3pa
+    self['3pa'] = self['3pa']  + self['3p'] 
+  end
+
+  def set_fta
+    self.fta = self.fta + self.ft
+  end
+
+  def set_sp
+    self.sp = self.sp / 60
+  end
+
+
 end
